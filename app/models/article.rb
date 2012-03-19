de0e7a6b9b15fce1d9105951c9aa82dedbf34761
Ulf Portner <ulf.portner@googlemail.com>
2012-03-19 15:07:02 +0100

    Add some article specs

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,9 @@ class Article
   include Mongoid::Document
 
   field :name, :type => String
+  field :buying_date => DateTime
+  field :barcode => String
 
-  attr_reader :name
+  # attr_reader :name, :buying_date
 
 end
--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -2,8 +2,39 @@ require 'spec_helper'
 
 describe Article do
 
-  it "should have a name" do
-    article = Article.new(:name => "Foo")
-    article[:name].should be_kind_of(String)
+  before :each do
+    date = DateTime.new(2001,2,3,4,5,6)
+    exp_date = DateTime.new(2001,2,3,4,5,6)
+    @article = Article.new(
+      :name => "Foo",
+      :buying_date => date,
+      :barcode => '12345',
+      :serial_no => '12345',
+      :expiration_date => exp_date,
+      :position => "not here")
   end
+  it "should have a name" do
+    @article[:name].should be_kind_of(String)
+  end
+
+  it "should have a buying date" do
+    @article[:buying_date].should be_kind_of(DateTime)
+  end
+
+  it "should have a barcode" do
+    @article[:barcode].should be_kind_of(String)
+  end
+
+  it "should have a serial number" do
+    @article[:serial_no].should be_kind_of(String)
+  end
+
+  it "should have an expiration date" do
+    @article[:expiration_date].should be_kind_of(DateTime)
+  end
+
+  it "should have a position" do
+    @article[:position].should be_kind_of(String)
+  end
+
 end
