1b508e19354155a726c724ce9932cfa3d47daa14
Ulf Portner <ulf.portner@googlemail.com>
2012-03-19 13:35:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warehouse::Application.config.secret_token = '981fa51aa8126c9a0c0a1ae393aac038bf87f1ef2e2d3a3826ef019f16fa09240babc3731b1f74543841baa802be109fc128c00332ce2a302bf2ddf7d43180ca'
