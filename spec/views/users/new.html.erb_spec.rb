4e1a4884b39da60d5be7c97b52cc7517865bf725
Thilo Grashei <elgrillo83@gmail.com>
2012-03-29 15:45:41 +0200

    Added user views show and new

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,15 +1,60 @@
 require 'spec_helper'
 
-describe "users/new" do
-  before(:each) do
-    assign(:user, stub_model(User).as_new_record)
+# describe "users/new" do
+#   before(:each) do
+#     assign(:user, stub_model(User).as_new_record)
+#   end
+
+#   it "renders new user form" do
+#     render
+
+#     # Run the generator again with the --webrat flag if you want to use webrat matchers
+#     assert_select "form", :action => users_path, :method => "post" do
+#     end
+#   end
+# end
+
+describe "users/new.html.erb" do
+  let(:user) do
+    mock_model("User").as_new_record.as_null_object
   end
 
-  it "renders new user form" do
-    render
+  before do
+    assign(:user, user)
+  end
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => users_path, :method => "post" do
+	it "renders a form to create a user" do
+		#assign(:user, mock_model("User").as_new_record.as_null_object)
+		render
+		rendered.should have_selector("form",
+			:method => "post",
+			:action => users_path
+		) do |form|
+		  form.should have_selector("input", :type => "submit")
+		end
+	end
+
+  it "renders a text field for the user name" do
+    user.stub(:name => "user name")
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("input",
+        :type => "text",
+        :name => "user[name]",
+        :value => "user name"
+      )
+    end
+  end
+
+  it "renders a text field for the user email" do
+    user.stub(:email => "user@mail.com")
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("input",
+        :type => "text",
+        :name => "user[email]",
+        :value => "user@mail.com"
+        )
     end
   end
 end
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
-describe "users/show" do
-  before(:each) do
-    @user = assign(:user, stub_model(User))
-  end
+# describe "users/show" do
+#   before(:each) do
+#     @user = assign(:user, stub_model(User))
+#   end
 
-  it "renders attributes in <p>" do
-    render
-  end
+#   it "renders attributes in <p>" do
+#     render
+#   end
+# end
+ 
+describe "users/show.html.erb" do
+	it "displays the name attribute of the user" do
+		# creates test double which stubs name() method with
+		# return value "joe" & assigns it to @user instance 
+		# vaiable on the view
+		assign(:user, double("Username", :name => "joe"))
+		render
+		rendered.should contain("joe")
+	end
 end
