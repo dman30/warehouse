e9773fda2293b2f7fad7325acb774863809b0f82
Ulfmann <myemail@me.com>
2012-03-29 11:31:05 +0200

    Use username field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
-  
+
+  field :username, :type => String
   validates_presence_of :username
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe User do
 
-	context "A user (in general)" do
-		before :each do
-			@user = User.new
-		end
-		
-		it "should be invalid without a username" do
-			@user.should_not be_valid
-		end
-		
-		it "should be valid with username" do
-			@user.username = 'someusername'
-			@user.should be_valid
-		end
-	end
+  context "Validation" do
+    before :each do
+      @user = User.new
+    end
+
+    it "should be invalid without a username" do
+      @user.should have(1).error_on(:username)
+    end
+
+    it "should be valid with username" do
+      @user.username = 'someusername'
+      @user.should have(:no).error_on(:username)
+    end
+  end
 end
