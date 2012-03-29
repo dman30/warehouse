221cd8c5412f1f59154742ee8952dfc07aaab600
Thilo Grashei <elgrillo83@gmail.com>
2012-03-29 15:44:58 +0200

    Actually nothing changed, git just thought so..

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User
   
   attr_accessor :password
 
-  field :username, :type => String
-  field :email, :type => String
-  field :password, :type => String
+  	field :username, :type => String
+  	field :email, :type => String
+  	field :password, :type => String
   
-  validates_presence_of :username, :message => "is required"
+  	validates_presence_of :username, :message => "is required"
 	validates_presence_of :email, :message => "is required"
 	validates_presence_of :password
 	
