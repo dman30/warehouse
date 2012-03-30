16b4c202ecc19f372434c27b2ada65dd319f7dd1
Thilo Grashei <elgrillo83@gmail.com>
2012-03-30 17:07:58 +0200

    Added attributes specs

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,8 +2,26 @@ require 'spec_helper'
 
 describe Employee do
 
-  it "should know its articles" do
-    employee = Employee.new
-    employee.articles.should be_kind_of(Array)
+	before :each do
+		@employee = Employee.new(
+			:location => "room",
+			:contact => mock_model("Contact").as_null_object
+		)
+	end
+
+	context "Attributes" do
+  	it "should be valid with all valid attributes" do
+  		@employee.should be_valid
+		end
+
+		it "should not be valid without a location" do
+			@employee.location = nil
+			@employee.should_not be_valid
+		end
+
+		it "should not be valid without a contact" do
+			@employee.contact = nil
+			@employee.should_not be_valid
+		end
   end
 end
