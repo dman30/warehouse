f20943c7b7e6ec2aca5a89fa2d82a9718649b55c
Thilo Grashei <elgrillo83@gmail.com>
2012-03-30 14:41:56 +0200

    Changed user validators from name, email, contact to email, password, contact

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User
   include Mongoid::Document
-  
-  attr_accessor :password
 
-  	field :username, :type => String
-  	field :email, :type => String
-  	field :password, :type => String
-  
-  	validates_presence_of :username, :message => "is required"
-		validates_presence_of :email, :message => "is required"
-		validates_presence_of :password
-	
+  belongs_to :contact
+
+  validates_presence_of :email, :password, :contact
+  	
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,31 @@
 require 'spec_helper'
 
 describe User do
-
-  context "Validation" do
-    before :each do
-      @user = User.new
-    end
-
-    it "should be invalid without a username" do
-    	@user.email = 'joe@bloggs.com'
-    	@user.password = 'abcdefg'
-      @user.should_not be_valid
-      @user.errors[:username].first.should eq("is required")
-      @user.username = 'someusername'
-      @user.should be_valid
-    end
-    
-    it "should be invalid without an email" do
-    	@user.username = 'joebloggs'
-    	@user.password = 'abcdefg'
-    	@user.should_not be_valid
-    	@user.errors[:email].first.should eq("is required")
-    	@user.email = 'joe@bloggs.com'
-    	@user.should be_valid
-    end
-    
-    it "should be invalid without a password" do
-    	@user.email = 'joe@bloggs.com'
-    	@user.username = 'joebloggs'
-    	@user.should_not be_valid
-    	@user.password = 'abcdefg'
-    	@user.should be_valid
-   	end
+  before :each do
+    @user = User.new(
+      :email => "user@mail.com",
+      :password => "secret", 
+      :contact => mock_model("Contact").as_null_object
+    )
   end
+
+  it "is valid with valid attributes" do
+    @user.should be_valid
+  end
+  
+  it "is not valid without an email" do
+    @user.email = nil
+    @user.should_not be_valid
+  end
+
+  it "is not valid without a password" do
+    @user.password = nil
+    @user.should_not be_valid
+  end
+
+  it "is not valid without a contact" do
+    @user.contact = nil
+    @user.should_not be_valid
+  end
+
 end
