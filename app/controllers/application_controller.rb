b7ab9f73583e7c5925acb61432b6d9c5de5000d2
Thilo Grashei <elgrillo83@gmail.com>
2012-03-30 16:01:13 +0200

    Removed ACCESS_DENIED because of "ApplicationController" error in Rails app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from AccessDenied, :with => :access_denied
+#   rescue_from AccessDenied, :with => :access_denied
 
-protected
+# protected
 
-	def access_denied
-		redirect_to "/401.html"
-	end
+# 	def access_denied
+# 		redirect_to "/401.html"
+# 	end
 
 end
