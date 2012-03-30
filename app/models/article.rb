ca7bc1ff862d0b01a50afb06d0573f923be0a17a
Thilo Grashei <elgrillo83@gmail.com>
2012-03-30 18:18:54 +0200

    Added "getter"

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,39 @@
 class Article
   include Mongoid::Document
 
-  belongs_to :producer
-  belongs_to :distributor
-  belongs_to :employee
+  # belongs_to :producer
+  # belongs_to :distributor
+  # belongs_to :employee
 
-  validates_presence_of :name, :serial_no, :barcode, :producer, :distributor,
-  	:purchase_date, :warranty_expiration
+  # validates_presence_of :category, :name, :serial_no, :barcode, :producer, :distributor,
+  # 	:purchase_date, :warranty_expiration
+
+  def category
+  	@category
+  end
+
+  def name
+  	@name
+  end
+
+  def serial_no
+  	@serial_no
+  end
+
+  def barcode
+  	@barcode
+  end
+
+  def purchase_date
+  	@purchase_date
+  end
+
+  def warranty_expiration
+  	@warranty_expiration
+  end
+
+  def note
+  	@note
+  end
 
 end
