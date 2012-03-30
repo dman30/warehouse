830693dbf35671034fa29cb4daffa2d205614bdc
Thilo Grashei <elgrillo83@gmail.com>
2012-03-30 16:06:13 +0200

    Added validators

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,7 @@ class Article
   belongs_to :distributor
   belongs_to :employee
 
+  validates_presence_of :name, :serial_no, :barcode, :producer, :distributor,
+  	:purchase_date, :warranty_expiration
+
 end
