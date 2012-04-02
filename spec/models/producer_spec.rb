0981c286c657f620662283b5d2159236cf93d499
Thilo Grashei <elgrillo83@gmail.com>
2012-04-02 18:39:18 +0200

    Adds producer specs

--- spec/models/producer_spec.rb
+++ spec/models/producer_spec.rb
@@ -1,1 +1,29 @@
 require 'spec_helper'
+
+describe Producer do
+	
+	before :each do
+    @article = Article.new(
+      :category => "category",
+      :name => "name",
+      :barcode => "01010",
+      :serial_no => "a12b3",
+      :purchase_date => "01.01.2012",
+      :warranty_expiration => "01.01.2014"
+      )
+    end
+
+	it "should have Articles" do
+		article1 = Factory(:article)
+		article2 = Factory(:article)
+		article3 = Factory(:article)
+
+		debugger
+		producer = Producer.create
+		producer.articles << [article1, article2, article3]
+		producer.save
+
+		producer.articles.count.should == 3
+	end
+	# it { should have_many(:articles) }
+end
