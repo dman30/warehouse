95f302078551eae5a1ec6ec7ff9502d942f1dda1
Thilo Grashei <elgrillo83@gmail.com>
2012-04-03 18:30:36 +0200

    Changes validation specs

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,24 +2,29 @@ require 'spec_helper'
 
 describe Contact do
 
-  before :each do
-    @contact = Contact.new(
-      :company_name => "Kaeuferportal",
-      :first_name => "Max",
-      :last_name => "Mustermann",
-      :email => "abc@kp.de",
-      :website => "http://www.kaueferportal.de",
-      :street1 => "Musterstr. 1",
-      :street2 => "App. 1",
-      :country => "Deutschland",
-      :zipcode => "12345",
-      :town => "Berlin",
-      :note => "Super")
-  end
+  context "Validation" do
+    before :each do
+      # @contact = FactoryGirl.create(:contact)
+      @contact = Contact.create
+    end
 
-  %w[company_name first_name last_name email website street1 street2 country zipcode town note].each do |attribute|
-    it "should have a #{attribute}" do
-      @contact.send(attribute).should_not be_nil
+    [ :gender,
+      :first_name, 
+      :last_name, 
+      :company_name,
+      :phone, 
+      :mobile, 
+      :email,
+      :street1,
+      :zipcode,
+      :town,
+      :country
+    ].each do |attribute|
+
+      it "should not be valid without a #{attribute}" do
+        # @contact.attribute = nil
+        @contact.should have(1).error_on(attribute)
+      end
     end
   end
 end
