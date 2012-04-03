838cbe3a71dca9643b4b4988304e9cb3955d5a59
Thilo Grashei <elgrillo83@gmail.com>
2012-04-03 13:41:10 +0200

    Adds user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -19,4 +19,12 @@ FactoryGirl.define do
   factory :distributor do |f|
   	f.name 'Hallo'
   end
+
+  # http://railscasts.com/episodes/158-factories-not-fixtures?autoplay=true
+  factory :user do |f|
+    f.sequence(:username) { |n| "user#{n}" }
+    f.password "secret"
+    f.password_confirmation { |u| u.password }
+    f.sequence(:email) { |n| "user#{n}@mail.com" }
+  end
 end
