8cf7139ab1aeb92936987329c0d373c6ec064f71
Thilo Grashei <elgrillo83@gmail.com>
2012-04-03 18:29:59 +0200

    Adds fields and validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,5 +4,20 @@ class Contact
   has_many :employees
   has_many :distributors
   has_many :producers
- 
+
+  field :gender, :type => String
+  field :first_name, :type => String
+  field :last_name, :type => String
+  field :company_name, :type => String
+  field :phone, :type => String
+  field :mobile, :type => String
+  field :email, :type => String
+  field :street1, :type => String
+  field :zipcode, :type => String
+  field :town, :type => String
+ 	field :country, :type => String
+
+ 	validates_presence_of :gender, :first_name, :last_name, 
+ 		:company_name, :phone, :mobile,	:email, :street1,	
+ 		:zipcode, :town, :country
 end
