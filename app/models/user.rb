ad8e3b0221a60479eb8b8ca3452964f81188debc
Thilo Grashei <elgrillo83@gmail.com>
2012-04-03 13:42:24 +0200

    Adds validators for uniqueness, format_of, confirmation of and length_of

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User
 
   belongs_to :contact
 
-  validates_presence_of :email, :password, :contact
+	# http://railscasts.com/episodes/158-factories-not-fixtures?autoplay=true
+
+  validates_presence_of :email, :contact
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_presence_of :password, :on => :create
+  validates_confirmation_of :password
+  validates_length_of :password, :minimum => 6, :allow_blank => true
   	
 end
