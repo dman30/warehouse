985c87f951e75c3b18e15f5ae60608128d59bd0b
Thilo Grashei <elgrillo83@gmail.com>
2012-04-03 17:22:21 +0200

    Adds distributor specs for validation, response and association

---
+++ spec/models/distributor_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Distributor do
+	before :all do
+		Producer.delete_all
+    Distributor.delete_all
+  end
+
+	context "Validation" do
+		before :each do
+			@distributor = Factory(:distributor)
+		end
+
+		it "should not be valid without a name" do
+			@distributor.name = nil
+			@distributor.should have(1).error_on(:name)
+		end
+	end
+
+	context "Response" do
+    before :each do
+      @distributor = Factory(:distributor)
+    end
+
+    it "should respond to note" do
+      @distributor.should respond_to(:note)
+    end
+  end
+
+  context "Association" do
+		it "should have Articles" do
+			created_articles = FactoryGirl.create_list(:article, 3)
+
+			distributor = FactoryGirl.create(:producer)
+			distributor.articles << created_articles
+			distributor.save
+
+			distributor.articles.count.should == 3
+		end
+	end
+
+end
