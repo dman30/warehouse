ed22d0364512cdfcc4f068eaf5b62ff0438804b4
Thilo Grashei <elgrillo83@gmail.com>
2012-04-11 18:06:56 +0200

    Adds has_one contact relationship and fields, removes presence validation of contact

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User
   include Mongoid::Document
 
-  belongs_to :contact
+  has_one :contact, as: :contactable, dependent: :delete
+
+  field :email
+  field :password
 
 	# http://railscasts.com/episodes/158-factories-not-fixtures?autoplay=true
 
-  validates_presence_of :email, :contact
+  validates_presence_of :email
   validates_uniqueness_of :email
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  # validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_presence_of :password, :on => :create
   validates_confirmation_of :password
   validates_length_of :password, :minimum => 6, :allow_blank => true
