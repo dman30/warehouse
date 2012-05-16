bbd782ecaa727bc522aff4ed9d71aeccd4255251
elgrillo83 <elgrillo83@gmail.com>
2012-05-16 15:41:09 +0200

    refactor edit action to render html with layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,11 +31,6 @@ class ArticlesController < ApplicationController
 	
 	def edit
 		@article = Article.find(params[:id])
-
-    respond_to do |format|
-      format.html { render :layout => false }
-      format.json { render :json => @article }
-    end
 	end
 	
 	def create
