e7b33d3210dcf5eabb4e21ab8aba01cb990e92f4
elgrillo83 <elgrillo83@gmail.com>
2012-05-18 15:01:33 +0200

    add kaminari pagination to articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_filter :authenticate_user!
 	
 	def index
-		@articles = Article.all
+		@articles = Article.page(params[:page]).per(5) #all
 
 		respond_to do |format|
 		format.html
